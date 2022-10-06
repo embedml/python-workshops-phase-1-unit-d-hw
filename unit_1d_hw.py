e66f8b55f8ed64393249b2f2b61f0abb3c16144f
TrentonWallis <tcwallis@ncsu.edu>
2022-10-06 17:06:00 -0400

    New: added some class problems, open to suggestions

--- unit_1d_hw.py
+++ unit_1d_hw.py
@@ -63,7 +63,7 @@ Create a function called string_return_I_am_a_string() that returns the string "
 
 #### Create a Function Below ####
 '''
-Create a function called add_one(), that takes in a parameter x, adds 1 then returns that value
+Create a function called add_one(), that takes in a parameter x, adds 1 then returns the value
 
 '''
 
@@ -94,14 +94,6 @@ Have the function return x
 Hint: Use a for loop to iterate through the list
 '''
 
-# Here is some code made for you
-
-def slope(m : float, x : list, b : float):
-    
-    return x
-
-
-
 
 ##################################
 
@@ -129,8 +121,11 @@ Create a function called default_args, that takes in parameter pre_pend and x
 
 :param pre_pend: (int) Integer we would like to prepend to the list x
 :param x: (list) List we want to prepend onto
+:return x: (list) List after prepend
 
 Have the default value for pre_pend be 5. Have the function prepend the value pre_pend to the list x.
+
+Return the list x.
 
 
 '''
@@ -138,8 +133,54 @@ Have the default value for pre_pend be 5. Have the function prepend the value pr
 ##################################
 
 
+##### Create a Class Below #######
+'''
+Create a class called Car, create an init method (constructor) that takes in 
+miles_per_gallon, and assigns it to the attribute `mpg`.
+
+'''
+
+##################################
+
+##### Create a Class Below #######
+'''
+Create a class called Dog, create an init method (constructor) that takes in breed, and assigns
+it to the attribute `breed`. Have init also take in weight, an assign it to the attribute `weight`.
+
+Then create a method (aka class function) called `get_breed`. It should return the
+`breed` attribute. 
+
+Then create a method called `get_weight`. It should return the `weight` attribute.
+
+Note: The get_x functions should not have an input. Ex: def get_weight():
+
+'''
+
+##################################
+
+
+
+
+##### Create a Class Below #######
+'''
+Create a class called Math_Operations
+
+Create a method called add, that takes in a and b, adds them together (a+b) and returns the value. 
+
+Create a method called sub, that takes in a and b, then subtracts b from a (b-a), and returns the value.
+
+Create a method called slope_intercept, that takes in an m, x, and b. Then 
+returns the output based on the slope equation (y = m * x + b)
+
+Create a method called pythagorean, which takes in a and b, squares a and b, takes the sum then returns 
+the square roots the summed values. (output = sqrt(a^2 + b^2))
+
+HINT: Use ** instead of ^ for python exponents.
+HINT HINT: You can raise variable and numbers to the negative exponent for roots
+
+'''
+
 
 if __name__ == "__main__":
-    '''Run your functions below to see what they are doing by calling print(<function name>())'''
-    print("Test your functions here!")
+    print("Test your functions at the bottom of this file!")
     pass
